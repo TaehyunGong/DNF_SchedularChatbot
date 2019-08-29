dd6092628860c8ce5fd0add7a32db6f529aa84d7
tony <tony950620@naver.com>
2019-08-29 09:40:10 +0900

    20190829 - Spring Boot Starter 초기생성 후 Push

---
+++ DNF_SchedularChatbot/src/main/java/com/thkong/dnfchatbot/DnfSchedularChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.thkong.dnfchatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnfSchedularChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnfSchedularChatbotApplication.class, args);
+	}
+
+}

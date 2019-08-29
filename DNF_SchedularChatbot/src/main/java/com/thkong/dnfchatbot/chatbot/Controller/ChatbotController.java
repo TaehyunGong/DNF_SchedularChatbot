6f356d8777ee6e86cbabbf3fb12a2212eb6a1f87
tony <tony950620@naver.com>
2019-08-29 18:25:11 +0900

    간단 카카오 request json 파싱 예제

--- DNF_SchedularChatbot/src/main/java/com/thkong/dnfchatbot/chatbot/Controller/ChatbotController.java
+++ DNF_SchedularChatbot/src/main/java/com/thkong/dnfchatbot/chatbot/Controller/ChatbotController.java
@@ -1,11 +1,17 @@
 package com.thkong.dnfchatbot.chatbot.Controller;
 
+import java.io.IOException;
+
+import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.thkong.dnfchatbot.chatbot.service.ChatbotService;
-import com.thkong.dnfchatbot.chatbot.vo.ItemGrade;
+import com.thkong.dnfchatbot.chatbot.vo.kakaoReq.Action;
+import com.thkong.dnfchatbot.chatbot.vo.kakaoReq.DetailParam;
+import com.thkong.dnfchatbot.chatbot.vo.kakaoReq.KakaoReq;
 
 @RestController
 public class ChatbotController {
@@ -13,9 +19,22 @@ public class ChatbotController {
 	@Autowired
 	ChatbotService service;
 	
-	@RequestMapping(value="/grade")
-	public ItemGrade test() {
-		return service.getGrade();
+	@RequestMapping(value="/test")
+	public String test(@RequestBody String req) {
+		
+		ObjectMapper mapper = new ObjectMapper();
+		KakaoReq ob = null;
+		try {
+			ob = mapper.readValue(req, KakaoReq.class);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Action action = ob.getAction();
+		DetailParam param = action.getDetailParams().get("armor");
+		
+		String setName = param.getValue();
+		
+		return setName;
 	}
 	
 }

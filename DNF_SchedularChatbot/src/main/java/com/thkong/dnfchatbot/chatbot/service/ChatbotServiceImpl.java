3be426fd1e9702824c2381c57bb221357a8d54cb
tony <tony950620@naver.com>
2019-09-02 17:53:20 +0900

    20190902 - 사용자가 아이템 옵션까지 조회할수 있도록 수정

--- DNF_SchedularChatbot/src/main/java/com/thkong/dnfchatbot/chatbot/service/ChatbotServiceImpl.java
+++ DNF_SchedularChatbot/src/main/java/com/thkong/dnfchatbot/chatbot/service/ChatbotServiceImpl.java
@@ -59,6 +59,7 @@ public class ChatbotServiceImpl implements ChatbotService {
 	public String toDayRating() throws IOException {
 		ObjectMapper objmap = new ObjectMapper();
 		
+		//미래를 보는 지혜 = ff3bdb021bcf73864005e78316dd961c
 		String responseMsg = connItemStatus("ff3bdb021bcf73864005e78316dd961c");
 		TodayRating eq = objmap.readValue(responseMsg, TodayRating.class);
 		String text = eq.getItemGradeName()+"("+eq.getItemGradeValue()+"%)";
@@ -88,7 +89,8 @@ public class ChatbotServiceImpl implements ChatbotService {
 		
 		List<ItemSetOption> equipList = dao.getSetEquipment(setName);
 		
-		String responseMsg = connItemStatus("ff3bdb021bcf73864005e78316dd961c");
+		//해당 세트 아이템의 가장 첫번째 아이템의 등급을 가져온다.
+		String responseMsg = connItemStatus(equipList.get(0).getItemId());
 		TodayRating eq = new ObjectMapper().readValue(responseMsg, TodayRating.class);
 		
 		String title = equipList.get(0).getSetItemName().replace("세트", "("+eq.getItemGradeValue()+"%)");
@@ -98,11 +100,11 @@ public class ChatbotServiceImpl implements ChatbotService {
 		List<Items> items = new ArrayList<Items>();
 		
 		for(ItemSetOption equip : equipList) {
-			items.add(getItems(equip));
+			items.add(getItems(equip, option));
 		}
 		
 		String res = new ResponseTemplate()
-						.addListItem(title,titleImageUrl, items)
+						.addListItem(title, titleImageUrl, items)
 						.build();
 		
 		return res;
@@ -113,14 +115,18 @@ public class ChatbotServiceImpl implements ChatbotService {
 	 * @date 2019. 9. 2.
 	 * @description 아이템 Id를 받아 오늘날의 등급정보를 가져온다.
 	 */
-	public Items getItems(ItemSetOption equip) throws IOException {
+	public Items getItems(ItemSetOption equip, DetailParam option) throws IOException {
 		List<Map<String, String>> mapList = new ArrayList<Map<String, String>>(); 
 		String[] arr = equip.getOption().split(",");
-		for(String option : arr) {
-			String[] a = option.split("\\+");
-			Map<String, String> m = new HashMap<String ,String>();
-			m.put(a[0], a[1]);
-			mapList.add(m);
+		/*
+		 * '지능+254,체력+246,정신력+134,힘+45' 와같이 데이터가 넘어오기때문에 
+		 * split으로 ","로 분열 후 다시 "+"로 분해 후 map으로 키=옵션명, 값=옵션값 으로 넣는다.
+		 */
+		for(String splitOne : arr) {
+			String[] splitTwo = splitOne.split("\\+");
+			Map<String, String> splitMap = new HashMap<String ,String>();
+			splitMap.put(splitTwo[0], splitTwo[1]);
+			mapList.add(splitMap);
 		}
 		Items item = new Items();
 		
@@ -131,14 +137,48 @@ public class ChatbotServiceImpl implements ChatbotService {
 
 		String description = "";
 		
-		for(Map<String, String> map : mapList) {
+		//만약에 사용자가 "영광 모속" 이라고 입력하면 모속이 option으로 들어가지만 "영광"일경우 null로 들어가기때문에 조건을 부여
+		if(option != null) {
+			String opt = option.getValue();
+			String maxStat = "";
+			String stat = "";
+			
+			//DB에서 가져온 max 옵션을 반복문으로 돌려서 사용자가 입력한 옵션을 찾아 maxStat으로 넣어준다.
+			for(Map<String, String> map : mapList) {
+				if(map.get(option.getValue()) != null) {
+					maxStat = map.get(opt);
+					break;
+				}
+			}
+			
+			//rest로 가져온 스탯에서 위와 동일하게 사용자가 입력한 옵션을 찾고 값을 가져온다.
 			for(ItemStatus status : eq.getItemStatus()) {
-				String value = map.get(status.getName());
-				if(value != null) {
-					description += shortName(status.getName())+" " + status.getValue() +"(+"+(Integer.parseInt(value) - Integer.parseInt(status.getValue()))+") ";
+				if(status.getName().equals(opt)) {
+					stat = status.getValue();
+					break;
+				}
+			}
+			
+			//DB에서 가져온값과 REST로 가져온 값이 모든 있을경우만 description을 추가해준다.
+			if(maxStat.length() != 0 && stat.length() != 0) {
+				description += opt + " " + stat +"(+"+(Integer.parseInt(maxStat) - Integer.parseInt(stat))+") ";
+			}
+			
+		}else {
+			//사용자가 아이템만 입력했을때는 모든 옵션을 다 뿌려준다.
+			for(Map<String, String> map : mapList) {
+				for(ItemStatus status : eq.getItemStatus()) {
+					String value = map.get(status.getName());
+					if(value != null) {
+						description += shortName(status.getName())+" " + status.getValue() +"(+"+(Integer.parseInt(value) - Integer.parseInt(status.getValue()))+") ";
+					}
 				}
 			}
 		}
+		
+		//만약 사용자가 입력한 옵션이 없을경우 "옵션 없을"으로 보여준다.
+		if(description.length() == 0)
+			description="옵션 없음";
 		
 		item.setTitle(equip.getItemName());
 		item.setImageUrl("https://img-api.neople.co.kr/df/items/"+equip.getItemId());
